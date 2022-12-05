0c94c32ec5456843d2e1ad3f1533e55056f93439
VaverCZ <vav94@proton.me>
2022-12-05 11:12:12 +0100

    first commit

---
+++ Models/Comment.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace MujHelpdesk.Models;
+
+public partial class Comment
+{
+    [Key]
+    public int Id { get; set; }
+
+    public int ReqId { get; set; }
+
+    [StringLength(50)]
+    public string UserName { get; set; } = null!;
+
+    public string Text { get; set; } = null!;
+
+    [Column(TypeName = "image")]
+    public byte[]? Img { get; set; }
+
+    [Column(TypeName = "datetime")]
+    public DateTime PostDate { get; set; }
+
+    [ForeignKey("ReqId")]
+    [InverseProperty("Comments")]
+    public virtual Request Req { get; set; } = null!;
+}
---
+++ Models/Request.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace MujHelpdesk.Models;
+
+public partial class Request
+{
+    [Key]
+    public int Id { get; set; }
+
+	[Required(ErrorMessage = "Prosím vyplňte nadpis")]
+	[StringLength(50)]
+    public string Title { get; set; } = null!;
+
+	[Required(ErrorMessage = "Prosím popište problém")]
+	public string Text { get; set; } = null!;
+
+
+	[StringLength(50)]
+    public string Username { get; set; } = null!;
+
+    public int Status { get; set; }
+
+    [Column(TypeName = "datetime")]
+    public DateTime OrigDate { get; set; }
+
+    [Column(TypeName = "image")]
+	public byte[]? Img { get; set; }
+
+	[InverseProperty("Req")]
+    public virtual ICollection<Comment> Comments { get; } = new List<Comment>();
+}
